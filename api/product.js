851154a5323f2c7657d311dbb67adf9b68f35d93
柴梦妃 <chaimengfei@dserspro.com>
2025-06-12 19:10:33 +0800

    商品列表路由

--- api/product.js
+++ api/product.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://192.168.99.142:8009'
 export const getProductList = () => {
   return new Promise((resolve, reject) => {
     uni.request({
-      url: `${BASE_URL}/api/products`,
+      url: `${BASE_URL}/api/product/list`,
       method: 'GET',
       success: (res) => {
         resolve(res.data)
